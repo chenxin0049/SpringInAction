3acc74ba33345a9eadad36b949982c4a70b4ebb4
xin <chenxin0049@gmail.com>
2019-08-15 21:33:23 +0800

    bean配置

---
+++ src/main/java/com/springinaction/other/ScopedBeanConfig.java
@@ -0,0 +1,9 @@
+package com.springinaction.other;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ScopedBeanConfig {
+}
