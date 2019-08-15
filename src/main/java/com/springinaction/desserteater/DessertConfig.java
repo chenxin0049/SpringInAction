cb8106898be9d1e1849244831711e8fc16027fb7
xin <chenxin0049@gmail.com>
2019-08-15 20:41:15 +0800

    @Qualifier的使用

---
+++ src/main/java/com/springinaction/desserteater/DessertConfig.java
@@ -0,0 +1,9 @@
+package com.springinaction.desserteater;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class DessertConfig {
+}
