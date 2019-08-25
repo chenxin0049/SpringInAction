db5af21e6907c36006e96be5374d6ab07ae8672b
xin <chenxin0049@gmail.com>
2019-08-25 09:20:58 +0800

    扫描过滤

--- src/main/java/spittr/config/RootConfig.java
+++ src/main/java/spittr/config/RootConfig.java
@@ -3,9 +3,21 @@ package spittr.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
+
+import java.util.regex.Pattern;
 
 @Configuration
-@ComponentScan(basePackages = {"spittr"}, excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
+
+//启用扫描器 并过滤一些Resource
+@ComponentScan(basePackages = {"spittr"},
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.CUSTOM, value = RootConfig.WebPackage.class)})
 public class RootConfig {
+
+    //类名是否满足正则表达式
+    public static class WebPackage extends RegexPatternTypeFilter{
+        public WebPackage(){
+            super(Pattern.compile("spittr\\.web"));  // '\\' 插入一个正则表达式的反斜线 所以其后的字符具有特殊的意义 这里指spittr.web
+        }
+    }
 }
