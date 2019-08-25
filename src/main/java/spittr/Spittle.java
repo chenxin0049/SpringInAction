076c68f54488fde9b74d15d870144b41e3d8f66f
xin <chenxin0049@gmail.com>
2019-08-25 09:25:08 +0800

    Spittle实体

---
+++ src/main/java/spittr/Spittle.java
@@ -0,0 +1,38 @@
+package spittr;
+
+import java.util.Date;
+
+public class Spittle {
+    private final Long id;
+    private final String message;
+    private final Date time;
+    private Double latitude;
+    private Double longitude;
+
+    public Spittle(String message, Date time){
+        this(message, time, 0.0, 0.0);
+    }
+    public Spittle(String message, Date time, Double latitude, Double longitude){
+        this.id = null;
+        this.message = message;
+        this.time = time;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public Date getTime() {
+        return time;
+    }
+    public Double getLatitude() {
+        return latitude;
+    }
+    public Double getLongitude() {
+        return longitude;
+    }
+}
