510c5fba0334ca56a013a65da21080f49814ced2
xin <chenxin0049@gmail.com>
2019-08-25 17:25:58 +0800

    Tiles视图解析器

--- src/main/java/spittr/config/WebConfig.java
+++ src/main/java/spittr/config/WebConfig.java
@@ -7,19 +7,24 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("spittr.web")
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
+    public TilesConfigurer tilesConfigurer(){
+        TilesConfigurer tiles = new TilesConfigurer();
+        tiles.setDefinitions(new String[] { "/WEB-INF/layout/tiles.xml" });
+        tiles.setCheckRefresh(true);
+        return tiles;
+    }
+
+    @Bean
     public ViewResolver viewResolver(){
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        resolver.setExposeContextBeansAsAttributes(true);
-        return resolver;
+        return new TilesViewResolver();
     }
 
     @Override
