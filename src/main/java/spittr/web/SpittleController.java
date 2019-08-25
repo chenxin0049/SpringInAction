909cbd727409eee5bace4f31c96519cd78fa4c54
xin <chenxin0049@gmail.com>
2019-08-25 09:24:26 +0800

    Spittle控制器

---
+++ src/main/java/spittr/web/SpittleController.java
@@ -0,0 +1,39 @@
+package spittr.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import spittr.data.SpittleRepository;
+
+@Controller
+@RequestMapping("/spittles")
+public class SpittleController {
+    private SpittleRepository spittleRepository;
+
+    @Autowired
+    public SpittleController(SpittleRepository spittleRepository){
+        this.spittleRepository = spittleRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String spittles(Model model){
+        model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+        return "spittles";
+    }
+
+    @RequestMapping(value = "/show", method = RequestMethod.GET)
+    public String showSpittle(@RequestParam("spittle_id") int spittleId, Model model){
+        model.addAttribute(spittleRepository.findOne(spittleId));
+        return "spittle";
+    }
+
+    @RequestMapping(value = "/{spittleId}", method = RequestMethod.GET)
+    public String spittle(@PathVariable int spittleId, Model model){
+        model.addAttribute(spittleRepository.findOne(spittleId));
+        return "spittle";
+    }
+}
